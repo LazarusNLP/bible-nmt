658ce9c2042ff7e5ca6d030446ba560c202de84c
Davidsamuell <davidsamuel.7878@gmail.com>
2025-07-21 00:47:07 +1000

    Add iso code docs, update finetuning shell script:

--- run_train.sh
+++ run_train.sh
@@ -1,12 +1,17 @@
-python src/run_translation.py \
-    --model_name facebook/nllb-200-distilled-1.3B \
-    --dataset_name LazarusNLP/alkitab-sabda-mt \
-    --src_lang ind \
-    --tgt_lang btx \
-    --src_lang_nllb ind_Latn \
-    --tgt_lang_nllb btx_Latn \
-    --max_length 256 \
-    --per_device_train_batch_size 64 \
-    --per_device_eval_batch_size 16 \
-    --learning_rate 2e-4 \
-    --max_steps 5000
+`iso_codes=(jav sun mad bvz ban bug mak sda btx bts bbc mqj)
+
+for code in "${iso_codes[@]}"; do
+    echo "Running for target language: $code"
+    python src/run_translation.py \
+        --model_name facebook/nllb-200-distilled-1.3B \
+        --dataset_name LazarusNLP/alkitab-sabda-mt \
+        --src_lang ind \
+        --tgt_lang "$code" \
+        --src_lang_nllb ind_Latn \
+        --tgt_lang_nllb "${code}_Latn" \
+        --max_length 256 \
+        --per_device_train_batch_size 64 \
+        --per_device_eval_batch_size 16 \
+        --learning_rate 2e-4 \
+        --max_steps 5000 || { echo "Failed for $code, continuing..."; continue; }
+done`
