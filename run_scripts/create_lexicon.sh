c77b48260317d33b3c510c31d863b5956c4ff6d5
DavidSamuell <davidsamuel.7878@gmail.com>
2025-09-25 11:41:34 +1000

    added luang dictionary

--- run_scripts/create_lexicon.sh
+++ run_scripts/create_lexicon.sh
@@ -1,8 +1,8 @@
 
-fast_align -i ebible-corpus/dhao/aligned-ind-nfa-ot.txt -d -o -v -p ebible-corpus/dhao/conditional_probs.txt
+fast_align -i ebible-corpus/luang/aligned-eng-luang-all.txt -d -o -v -p ebible-corpus/luang/conditional_probs.txt
 
 python scripts/create_lexicon_from_conditional_probs.py \
-    ebible-corpus/dhao/conditional_probs.txt \
-    ebible-corpus/dhao/ot_words_confidence_score.csv \
+    ebible-corpus/luang/conditional_probs.txt \
+    ebible-corpus/luang/ot_words_confidence_score.csv \
     --include-null \
     --exclude-punctuation
